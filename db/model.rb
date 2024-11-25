c0ab1810cd67b13c31cf5d40d4769ef3a2834106
PYM5 <pymsoft5@gmail.com>
2024-11-25 17:19:07 +0100

    Model

---
+++ db/model.rb
@@ -0,0 +1,5 @@
+rails generate model trip name start_date:date end_date:date user:references destination:references
+rails generate model destination address currency papers food power
+rails generate model activity name description reviews:float address website_url wiki
+rails generate model trip_partner user:references trip:references
+# rails generate model follower user:references follower:references
