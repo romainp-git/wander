646f3365e8327fee862fd368ae287deb6a1f252d
romain portier <romain.portier@wanadoo.fr>
2024-11-26 23:21:12 +0100

    date form improvments

--- app/javascript/controllers/datepicker_controller.js
+++ app/javascript/controllers/datepicker_controller.js
@@ -2,37 +2,50 @@ import { Controller } from "@hotwired/stimulus"
 
 // Connects to data-controller="datepicker"
 export default class extends Controller {
-  static targets = ["startDate", "endDate"]; // Liens vers les champs masqués
+  static targets = ["startDate", "endDate", "calendar"]; // Liens vers les champs masqués
 
   connect() {
-    console.log("hello")
-    // Initialisation de Flatpickr (ou un autre sélecteur de date)
+    console.log("flatpicker_controller")
     this.initializeDatepicker();
   }
 
   initializeDatepicker() {
     flatpickr(this.element, {
+      minDate: "today",
+      appendTo: this.calendarTarget,
       mode: "range",
       dateFormat: "Y-m-d",
+      locale: {
+        firstDayOfWeek: 1,
+        weekdays: {
+          shorthand: ['Dim', 'Lun', 'Mar', 'Mer', 'Jeu', 'Ven', 'Sam'],
+          longhand: ['Dimanche', 'Lundi', 'Mardi', 'Mercredi', 'Jeudi', 'Vendredi', 'Samedi'],
+        },
+        months: {
+          shorthand: ['Jan', 'Fév', 'Mar', 'Avr', 'Mai', 'Juin', 'Juil', 'Août', 'Sept', 'Oct', 'Nov', 'Déc'],
+          longhand: ['Janvier', 'Février', 'Mars', 'Avril', 'Mai', 'Juin', 'Juillet', 'Août', 'Septembre', 'Octobre', 'Novembre', 'Décembre'],
+        },
+      },
       inline: true,
       altFormat: "D/M/Y",
       onChange: this.updateFields.bind(this),
     });
+
+    flatpickr.localize(flatpickr.l10ns.fr);
   }
 
   updateFields(selectedDates) {
     if (selectedDates.length === 2) {
       const [start, end] = selectedDates; // Extraire les dates de début et de fin
-      document.getElementById("start-date").value = this.formatDate(start);
-      document.getElementById("end-date").value = this.formatDate(end);
+      this.startDateTarget.textContent  = this.formatDate(start);
+      this.endDateTarget.textContent = this.formatDate(end);
     }
   }
 
   formatDate(date) {
-    // Formater la date en "YYYY-MM-DD"
     const year = date.getFullYear();
     const month = String(date.getMonth() + 1).padStart(2, "0");
     const day = String(date.getDate()).padStart(2, "0");
-    return `${year}-${month}-${day}`;
+    return `${day}-${month}-${year}`;
   }
 }
