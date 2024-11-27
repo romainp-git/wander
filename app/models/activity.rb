c1871a36b165d2321a8befeae5456c21bdebd4ae
PYM5 <pymsoft5@gmail.com>
2024-11-27 09:06:27 +0100

    IA Trip Activities

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -2,6 +2,10 @@ class Activity < ApplicationRecord
   has_many :trip_activities
   has_many :trips, through: :trip_activities
   has_many_attached :photos
-  geocoded_by :address
-  after_validation :geocode, if: :will_save_change_to_address?
+
+  # A VALIDER ENSEMBLE
+  # La latitude et la longitude sont fournies par openai et à priori on ne modifie pas une adresse d'activité donc pas besoin de mise à jour par geocoder
+
+  # geocoded_by :address
+  # after_validation :geocode, if: :will_save_change_to_address?
 end
