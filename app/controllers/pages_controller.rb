14bdddec148330c15ec75432bac07eec5beb0132
aurelien bernard <aurelien.bernard85@orange.fr>
2024-11-29 12:07:35 +0100

    ajout des stats

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,9 +2,17 @@ class PagesController < ApplicationController
 
   def home
     @trips = Trip.where(user: current_user)
-    @destinations = self.trips.empty? ? ["FRA", "GBR"] : trips
+    @destinations = self.trips.empty? ? ["FRA","FRA","GBR","NLD","AUS"] : trips
+    @travels = self.journeys.empty? ? ["FRA","FRA","GBR","NLD","AUS"] : journeys
+    @stats = {
+      total_countries_visited: @destinations.count,
+      ratio: ((@destinations.count / 249.0) * 100).round(2),
+      total_travels: @travels.count
+    }
   end
-
+  def journeys
+    @trips.map(&:destination).map(&:alpha3code).compact
+  end
 
   def trips
     @trips.map(&:destination).map(&:alpha3code).compact.uniq
