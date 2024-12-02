c9711d226fe34e1a3e11bd45109813681790674d
Romain Portier <romain.portier@wanadoo.fr>
2024-12-02 11:07:04 +0100

    sidekiq

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,23 +1,7 @@
 Sidekiq.configure_server do |config|
-  config.redis = {
-    url: 'rediss://:REDACTED@ec2-108-128-57-24.eu-west-1.compute.amazonaws.com:22060',
-    ssl: true,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE }
-  }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = {
-    url: ENV.fetch('REDIS_URL', 'rediss://:REDACTED@ec2-108-128-57-24.eu-west-1.compute.amazonaws.com:22060'),
-    ssl: true,
-    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Désactive la vérification du certificat si nécessaire
-  }
+  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
 end
-
-# Sidekiq.configure_server do |config|
-#   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-# end
-
-# Sidekiq.configure_client do |config|
-#   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-# end
