5cd8642a1fc3395eeda0fa20a42ab18bf175a5c5
PYM5 <pymsoft5@gmail.com>
2024-12-02 15:55:08 +0100

    conf5

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,15 +1,13 @@
-# redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/")
-
 Sidekiq.configure_server do |config|
   config.redis = {
-    url: ENV['REDIS_URL'],
+    url: ENV['REDIS_TEMPORARY_URL'],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Désactive la vérification SSL
   }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = {
-    url: ENV['REDIS_URL'],
+    url: ENV['REDIS_TEMPORARY_URL'],
     ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Désactive la vérification SSL
   }
 end
