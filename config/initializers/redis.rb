7461158c529ae5ae9b53f8b134e78f32d5da60bb
PYM5 <pymsoft5@gmail.com>
2024-12-02 14:19:39 +0100

    initializer redis

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,4 +1,8 @@
-$redis = Redis.new(
-  url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1'), 
-  ssl_params: { 
-    verify_mode: OpenSSL::SSL::VERIFY_NONE })
+redis_url = ENV.fetch("REDIS_URL", "redis://localhost:6379/")
+Sidekiq.configure_server do |config|
+  config.redis = { url: redis_url }
+end
+
+Sidekiq.configure_client do |config|
+  config.redis = { url: redis_url }
+end
