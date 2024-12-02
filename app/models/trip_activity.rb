723ca1c27fca316ca25b046f1ff76d08a2d3e280
samuel willem <samuelwillem@hotmail.fr>
2024-12-02 01:33:54 +0100

    implement drageable activities

--- app/models/trip_activity.rb
+++ app/models/trip_activity.rb
@@ -1,4 +1,5 @@
 class TripActivity < ApplicationRecord
+  acts_as_list scope: [:trip_id, :start_date]
   belongs_to :activity
   belongs_to :trip
 end
--- config/routes.rb
+++ config/routes.rb
@@ -15,5 +15,7 @@ Rails.application.routes.draw do
   resources :suggestions, only: %i[show]
   resources :searches, only: [:new, :create, :show]
 
+  resources :trip_activities, only: [:update, :index]
+
   get "up" => "rails/health#show", as: :rails_health_check
 end
