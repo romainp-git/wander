0f9c7a2d8a2ffc2770d90d08f541fe32e1c01e2f
PYM5 <pymsoft5@gmail.com>
2024-12-02 10:26:06 +0100

    Sidekiq.rb

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,7 +1,23 @@
 Sidekiq.configure_server do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = { 
+    url: 'rediss://:REDACTED@ec2-108-128-57-24.eu-west-1.compute.amazonaws.com:22060', 
+    ssl: true, 
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } 
+  }
 end
 
 Sidekiq.configure_client do |config|
-  config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+  config.redis = {
+    url: ENV.fetch('REDIS_URL', 'rediss://:REDACTED@ec2-108-128-57-24.eu-west-1.compute.amazonaws.com:22060'),
+    ssl: true,
+    ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } # Désactive la vérification du certificat si nécessaire
+  }
 end
+
+# Sidekiq.configure_server do |config|
+#   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+# end
+
+# Sidekiq.configure_client do |config|
+#   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
+# end
