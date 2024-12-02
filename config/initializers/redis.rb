e6a595e52425a9c23bf729a3423678bc12595db7
PYM5 <pymsoft5@gmail.com>
2024-12-02 12:59:38 +0100

    redis nossl

--- config/initializers/redis.rb
+++ config/initializers/redis.rb
@@ -1,1 +1,4 @@
-$redis = Redis.new(url: ENV["REDIS_URL"], ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE })
+$redis = Redis.new(
+  url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1'), 
+  ssl_params: { 
+    verify_mode: OpenSSL::SSL::VERIFY_NONE })
