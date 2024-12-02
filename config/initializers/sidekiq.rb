68d64dc0ef6a2c783bfbca57b38c2de0cb2e2579
PYM5 <pymsoft5@gmail.com>
2024-12-02 12:40:16 +0100

    redis ssl

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -1,10 +1,12 @@
 Sidekiq.configure_server do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  # config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER } }
 end
 
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV.fetch('REDIS_URL', 'redis://localhost:6379/1') }
-  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  # config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_NONE } }
+  config.redis = { ssl_params: { verify_mode: OpenSSL::SSL::VERIFY_PEER } }
 end
 
