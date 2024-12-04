69655a0c42ec0a337a3519d373c07020926592ad
Romain Portier <romain.portier@wanadoo.fr>
2024-12-04 09:18:03 +0100

    mmmm

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -6,7 +6,16 @@ class Activity < ApplicationRecord
 
   # A VALIDER ENSEMBLE
   # La latitude et la longitude sont fournies par openai et à priori on ne modifie pas une adresse d'activité donc pas besoin de mise à jour par geocoder
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 
-   geocoded_by :address
-   after_validation :geocode, if: :will_save_change_to_address?
+  after_update_commit :broadcast_if_complete
+
+  private
+
+  def broadcast_if_complete
+    if description.present? && photos.attached?
+      broadcast_replace_to "activities"
+    end
+  end
 end
