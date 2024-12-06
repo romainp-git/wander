f0094fc6a416a048f5081dbdb603ab0d5f19e870
aurelien bernard <aurelien.bernard85@orange.fr>
2024-12-06 12:34:05 +0100

    creation seed

--- app/javascript/controllers/countup_controller.js
+++ app/javascript/controllers/countup_controller.js
@@ -1,18 +1,17 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["years", "days", "hours", "minutes", "seconds"];
+  static targets = ["days", "hours", "minutes", "seconds"];
   static values = { startDate: String };
 
   connect() {
-    // Récupère la date de début du dataset
     this.startDate = new Date(this.startDateValue);
-    this.updateTimer(); // Met à jour immédiatement au chargement
-    this.timer = setInterval(() => this.updateTimer(), 1000); // Met à jour toutes les secondes
+    this.updateTimer();
+    this.timer = setInterval(() => this.updateTimer(), 1000);
   }
 
   disconnect() {
-    clearInterval(this.timer); // Nettoie le timer lors de la déconnexion du contrôleur
+    clearInterval(this.timer);
   }
 
   updateTimer() {
@@ -23,16 +22,14 @@ export default class extends Controller {
     let remainingSeconds = timeDifferenceInSeconds % (365 * 24 * 60 * 60);
 
     const days = Math.floor(remainingSeconds / (24 * 60 * 60));
-    remainingSeconds %= (24 * 60 * 60);
+    remainingSeconds %= 24 * 60 * 60;
 
     const hours = Math.floor(remainingSeconds / (60 * 60));
-    remainingSeconds %= (60 * 60);
+    remainingSeconds %= 60 * 60;
 
     const minutes = Math.floor(remainingSeconds / 60);
     const seconds = remainingSeconds % 60;
 
-    // Mise à jour des targets dans le DOM
-    this.yearsTarget.textContent = years;
     this.daysTarget.textContent = days;
     this.hoursTarget.textContent = this.padZero(hours);
     this.minutesTarget.textContent = this.padZero(minutes);
